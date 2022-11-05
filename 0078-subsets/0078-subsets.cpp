2ebaa7e6b35374b7bc3496eddbe5a85aa3b604a3
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2022-11-05 10:33:21 +0530

    Time: 3 ms (77.60%), Space: 16.7 MB (19.25%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    
+    private:
+      void solve(vector<int> nums , vector<int> output , int i , vector<vector<int>>& ans ){
+        // base case
+        if(i >= nums.size()){
+            
+            ans.push_back(output);
+            return;
+        }
+        
+        
+        
+        //exclude
+        solve(nums , output , i+1 , ans);
+        
+        
+        //include
+         
+        output.push_back(nums[i]);
+        solve(nums , output , i+1 , ans);
+
+    }
+    
+public:
+ 
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> ans ;
+        vector<int> output;
+        int i = 0 ;
+        
+        solve(nums , output , i , ans);
+        
+        return ans; 
+        
+    }
+};
