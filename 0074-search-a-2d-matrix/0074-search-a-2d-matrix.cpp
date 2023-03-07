716749117f28c9b7a8177c8c95b859ee24d4b2f2
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-03-07 11:50:25 +0530

    Time: 3 ms (86.32%), Space: 9.6 MB (59.15%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // vector<int> vect;
+        vector<int> vect;
         int m = matrix.size();
         int n = matrix[0].size();
         
-//         for(int i = 0 ;i<m ; i++){
-//             for(int j = 0 ; j < n ; j++){
-//                 vect.push_back(matrix[i][j]);
-//             }}
-        
-//         int size = vect.size();
-//         int s = 0 ;
-//         int e = size-1;
-//         int mid = 0;
-//         while(e>=s){
-//             mid = (e+s)/2;
-//             if(vect[mid] == target){
-//                 return true;
-//             }else if(vect[mid] > target){
-//                 e = mid-1;
-//             }
-//             else{
-//                 s = mid + 1;
-//             }
-//         }
-        
-//         return false;
-        
-              for(int i = 0 ;i<m ; i++){
+        for(int i = 0 ;i<m ; i++){
             for(int j = 0 ; j < n ; j++){
-               if (matrix[i][j] == target){
-                   return true;
-               }
+                vect.push_back(matrix[i][j]);
             }}
+        
+        int size = vect.size();
+        int s = 0 ;
+        int e = size-1;
+        int mid = 0;
+        while(e>=s){
+            mid = (e+s)/2;
+            if(vect[mid] == target){
+                return true;
+            }else if(vect[mid] > target){
+                e = mid-1;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        
         return false;
+        
+        //       for(int i = 0 ;i<m ; i++){
+        //     for(int j = 0 ; j < n ; j++){
+        //        if (matrix[i][j] == target){
+        //            return true;
+        //        }
+        //     }}
+        // return false;
     }
 };
