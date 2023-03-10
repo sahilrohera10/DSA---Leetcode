56fa01a65527d7eca784652d111fe6d97aa5328e
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-03-10 09:58:31 +0530

    Time: 26 ms (42.93%), Space: 23.7 MB (5.83%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
     
-    void reverse(vector<char>& s , int i , int j ){
+//     void reverse(vector<char>& s , int i , int j ){
         
-        //Base Case
-        if(i >= j){
-            return;
-        }
+//         //Base Case
+//         if(i >= j){
+//             return;
+//         }
         
-        swap(s[i] , s[j]);
+//         swap(s[i] , s[j]);
         
-        reverse(s , i+1 , j-1);
+//         reverse(s , i+1 , j-1);
         
         
-    }
+//     }
     
     
     void reverseString(vector<char>& s) {
         
         int n = s.size();
         
-        reverse(s , 0 , n-1);
+        //using recursion and swap
+        // reverse(s , 0 , n-1);
+        
+        // Now using stack
+        stack<char> st ;
+        for(int i = 0 ; i< n ; i++){
+            st.push(s[i]);
+        }
+        int i = 0;
+        
+        while(!st.empty()){
+            s[i] = st.top();
+            st.pop();
+            i++;
+        }
+        
+        
         
 
         
