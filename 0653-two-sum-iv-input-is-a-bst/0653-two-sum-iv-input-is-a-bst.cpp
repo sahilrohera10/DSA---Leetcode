eb4f27543561725b0732b71b1b63404c42741f76
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-03-31 12:42:50 +0530

    Time: 40 ms (71.98%), Space: 36.9 MB (50.14%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class BSTIterator {
+    stack<TreeNode*> st;
+    bool reverse = true;
+public:
+    BSTIterator(TreeNode* root , bool isReverse) {
+       reverse = isReverse; 
+       pushAll(root);
+        
+    }
+    
+    int next() {
+        TreeNode* element = st.top();
+        st.pop();
+        if(!reverse) pushAll(element->right);
+        else pushAll(element->left);
+        return element->val;
+        
+    }
+    
+    bool hasNext() {
+        return !st.empty();   
+    }
+    private : 
+      void pushAll(TreeNode* node){
+         while(node != NULL){
+             st.push(node);
+             if(reverse == true) node = node->right;
+             else node = node->left;
+           }
+        }   
+};
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        
+        
+        
+        //approach 1 - can make a vector containing inorder traversal ... then itrate on the vector with 2 pointer and identify the pair
+        
+        // approach 2
+        
+        if(!root) return false;
+        BSTIterator l(root , false);
+        BSTIterator r(root , true);
+        
+        int i = l.next();
+        int j = r.next();
+  
+        while(i<j){
+     
+            if(i+j == k) return true;
+            else if(i+j<k) i = l.next();
+            else j = r.next();
+        }
+        return false;     
+    }
+};
