a388ffbf184055c75280066744d8b199186b01ed
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-04-14 11:19:49 +0530

    Time: 3 ms (71.01%), Space: 5.9 MB (69.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    //memoisation
     int f(int n , vector<int>& dp)
     {
         if(n<=1) return n;
@@ -12,8 +13,38 @@ public:
     
     
     
+    
+    
+    
     int fib(int n) {
-        vector<int> dp(n+1 , -1) ;  // step1 - declaring the dp vector
-        return f(n , dp);
+        // vector<int> dp(n+1 , -1) ;  // step1 - declaring the dp vector
+        // return f(n , dp);   // memoisation (sc - O(n) + O(n))
+        
+        //tabulation (sc - O(n)) more optimised
+//         dp[0] = 0 ; 
+//         dp[1] = 1 ;
+        
+//         for(int i = 2 ; i <= n ; i++){
+            
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+        
+//         return dp[n];
+        
+        //most optimised using variables
+        int prev2 = 0 ;
+        int prev = 1 ;
+        int cur = 0 ;
+        if(n==0) return prev2 ;
+        if(n == 1) return prev ;
+        for(int i = 2 ; i<=n ; i++){
+            cur = prev + prev2 ;
+            prev2 = prev ;
+            prev = cur ;
+        }
+        
+        return prev ;
+        
+        
     }
 };
