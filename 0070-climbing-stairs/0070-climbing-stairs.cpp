4513997bbe6cb75cb6006e8ddde5106d01445514
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-04-14 12:06:33 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (39.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,57 +1,31 @@
 class Solution {
 public:
     
-    
-    int solve(int n , vector<int>& dp){
-        if(n == 0){
-            return 1;
-        }
-        if(n < 0){
-            return 0;
-        }
+    int f(int n , vector<int>& dp)
+    {
+        if(n <=1) return 1 ;
         
-        if(dp[n] != -1){
-            return dp[n];
-        }
+        if(dp[n] != -1) return dp[n];
         
-        dp[n] = solve(n-1 , dp) + solve(n-2 , dp);
-        
-        return dp[n] ;
+        return dp[n] = f(n-1 , dp) + f(n-2 , dp);
     }
     
-    //GIVING TLE BCZ THIS QUESTION IS OF DP WE DO THIS LATTER
+    
+    
     int climbStairs(int n) {
-        
-        //DP BASED
-        vector<int> dp(n+1 , -1) ;
-        // for(int i = 0; i<=n ; i++){
-        //     dp.push_back(-1);
-        // }
-        
-        return solve(n , dp);
-        
-        
-        
-        //RECURSION BASED
-//         //if the starting stair is at lets suppose <0 return 0;
-//         //if the starting stair is at lets suppose == 0 return 1; means thier is only one way to             reach that only stair
-        
-//         //base case 
-//         if(n==0){
-//             return 1;
-//         }
-//         if(n<0){
-//             return 0;
-//         }
-        
-        
-//         // we take this prblm not from starting to top , rather we take this from top to bottom
-       
-//         //recursive case
-        
-//         //since we have to return the no. of cases then we consider that the man has come from the n-1th stair or from the n-2th stair
+        // normal recursion
+//         if(n==0) return 1 ;
+//         if(n==1) return 1 ;
         
 //         return climbStairs(n-1) + climbStairs(n-2);
-
+        
+        
+        //memoisation
+        vector<int> dp(n+1 , -1);
+        
+        return f(n , dp) ;
+        
+        
+        
     }
 };
