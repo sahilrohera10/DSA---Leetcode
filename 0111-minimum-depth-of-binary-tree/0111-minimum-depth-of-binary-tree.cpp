a835f1db97c4305d7c0049b97d3440c49d956900
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-05-27 23:34:12 +0530

    Time: 294 ms (36.78%), Space: 144.5 MB (96.73%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,39 +12,71 @@
 class Solution {
 public:
     
-    int find(TreeNode* root)
-    {
-        if(root == NULL)
-        {
-            return 0;
-        }
+//     int find(TreeNode* root)
+//     {
+//         if(root == NULL)
+//         {
+//             return 0;
+//         }
    
-        return 1+ min(minDepth(root->left) , minDepth(root->right));
+//         return 1+ min(minDepth(root->left) , minDepth(root->right));
      
         
-    }
-      int findMax(TreeNode* root)
-    {
-        if(root == NULL)
-        {
-            return 0;
-        }
+//     }
+//       int findMax(TreeNode* root)
+//     {
+//         if(root == NULL)
+//         {
+//             return 0;
+//         }
    
-        return 1+ max(minDepth(root->left) , minDepth(root->right));
+//         return 1+ max(minDepth(root->left) , minDepth(root->right));
      
         
+//     }
+    
+    
+    void find(TreeNode* root , int cnt , int& ans){
+        if(root->left == NULL && root->right == NULL){
+            cnt++;
+            ans = min(cnt , ans);
+            return ;
+        }
+    
+        
+        cnt++;
+        
+       if(root->left) find(root->left , cnt , ans);
+        
+       if(root->right) find(root->right , cnt , ans);
+        
+        
     }
+    
+    
+    
+    
     int minDepth(TreeNode* root) {
-        int ans = 0;
-        if(root && (!root->left || !root->right)){
-            ans = findMax(root);
-        }
-      else{
-          ans =  find(root);
-      }
+        
+        if(root == NULL) return 0;
+        
+        int cnt = 0 ;
+        int ans = INT_MAX ;
+        
+        find(root , cnt ,ans);
+        
+        return ans ;
+        
+//         int ans = 0;
+//         if(root && (!root->left || !root->right)){
+//             ans = findMax(root);
+//         }
+//       else{
+//           ans =  find(root);
+//       }
         
         
-     return ans;
+//      return ans;
         
     }
 };
