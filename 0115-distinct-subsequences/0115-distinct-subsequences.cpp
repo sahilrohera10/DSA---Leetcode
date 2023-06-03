10eb0f5c55f3d3c3201058bab89f641ed2147681
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-06-03 09:31:44 +0530

    Time: 72 ms (25.16%), Space: 30 MB (37.99%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -23,9 +23,28 @@ public:
         int n = s.size();
         int m = t.size();
         //memoization
-        vector<vector<int>> dp(n , vector<int> (m+1 , -1));
-        int ans = find(n-1 , m-1 , s,t , dp);
+//         vector<vector<int>> dp(n , vector<int> (m+1 , -1));
+//         int ans = find(n-1 , m-1 , s,t , dp);
         
-        return ans ;
+        // tabulation
+        vector<vector<double>> dp(n+1 , vector<double> (m+1 , 0));
+        
+        for(int i = 0 ;i<=n ; i++) dp[i][0] = 1;
+        for(int j = 1 ;j<=m ; j++) dp[0][j] = 0;
+        
+        
+        for(int i = 1 ; i<=n ; i++){
+            for(int j=1 ; j<=m ; j++)
+            {
+                 if(s[i-1] == t[j-1]) 
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        
+        return dp[n][m] ;
     }
 };
