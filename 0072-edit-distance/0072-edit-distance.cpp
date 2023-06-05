b3b99bfb12401ffb93fd85a4e5ed08d7d4f8364b
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-06-05 11:00:34 +0530

    Time: 305 ms (5.16%), Space: 400.3 MB (5.23%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int find(int i , int j , string w1 , string w2 , vector<vector<int>>& dp)
+    {
+        //base case
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        //recursive case
+        if(w1[i] == w2[j]) return 0 + find(i-1 , j-1, w1,w2, dp);
+        
+        int insert = 1+find(i,j-1,w1,w2,dp);
+        int remove = 1+find(i-1,j,w1,w2,dp);
+        int replace = 1+find(i-1,j-1,w1,w2,dp);
+        
+        return dp[i][j] = min(insert , min(remove,replace));
+    }
+    
+    
+    
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        
+        // optimising with memoization
+        vector<vector<int>> dp(n , vector<int>(m+1 , -1));
+        
+        int ans = find(n-1,m-1 , word1, word2, dp);
+        
+        return ans;
+    }
+};
