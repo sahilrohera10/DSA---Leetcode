bfe5910e680bfaf6dad2fe5cb091f7b95437f53b
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-06-23 22:33:30 +0530

    Time: 38 ms (36.97%), Space: 25.6 MB (17.01%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
-
     void rotate(vector<int>& nums, int k) {
-        
-        
-        //APPROACH 1
-        //Time Limit Exceed
-//         int s;
-//         int temp ;
-
-//         while(k--){
-//             s = nums.size();
-//             temp = nums[s-1];
-//             nums.pop_back();
-//             nums.insert(nums.begin() , temp);
-//         }
-
         int n = nums.size();
+        k = k%n ;
         vector<int> temp;
         
-        if(k==0 || k==n){
-            return;
-        }
-        
-        if(k>n){
-            k = k%n;
-        }
-        
-        for(int i = n-k ; i< n ;i++){
+        for(int i = n-k ; i<n ;i++){
             temp.push_back(nums[i]);
         }
-     
-          for(int i = 0 ; i < n-k ;i++){
-            temp.push_back(nums[i]);
+        for(int i = n-k-1 ;i>=0;i--){
+            nums[i+k] = nums[i];
         }
-        
-           for(int i = 0 ; i < n ;i++){
+        for(int i = 0 ; i<k ;i++){
             nums[i] = temp[i];
         }
         
