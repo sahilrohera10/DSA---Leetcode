194fc96a25f8f86409d22d8a50316e774dd29a35
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-07-01 18:51:16 +0530

    Time: 148 ms (55.57%), Space: 93.2 MB (73.92%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        
+        //new approach
+        int mini = prices[0];
         int n = prices.size();
-        
-        int b = 0;
-        int s = 0;
-        int profit = 0;
-        
-        int i = 1 ;
-        while(i<n){
-            if(prices[i] < prices[b]){
-                b = i ;
-                s = i;
-            }
-            if(prices[i] > prices[s]){
-                s = i ;
-            }
-            
-            int a = prices[s] - prices[b];
-            profit = max(profit , a);
-            
-            i++;
+        int cost = 0;
+        int profit = 0 ;
+        for(int i = 1 ; i<n ;i++){
+            cost = prices[i] - mini;
+            profit = max(profit , cost);
+            mini = min(mini , prices[i]);
         }
         
         return profit;
+        
+        
+        
+        
+        
+        
+        
+//         int n = prices.size();
+        
+//         int b = 0;
+//         int s = 0;
+//         int profit = 0;
+        
+//         int i = 1 ;
+//         while(i<n){
+//             if(prices[i] < prices[b]){
+//                 b = i ;
+//                 s = i;
+//             }
+//             if(prices[i] > prices[s]){
+//                 s = i ;
+//             }
+            
+//             int a = prices[s] - prices[b];
+//             profit = max(profit , a);
+            
+//             i++;
+//         }
+        
+//         return profit;
+        
+        
     }
 };
