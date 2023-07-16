eb6a07f4f4d9d57d3337cfd19a284406e054f300
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-07-16 14:44:37 +0530

    Time: 275 ms (15.69%), Space: 90.9 MB (35.46%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        
+          //tabulation - same code as of buy&sell stock II, with one condition that , we have to give a fee every time after a successful transaction.
+        
+        int n = prices.size();
+         vector<vector<long>> dp(n+1 , vector<long>(2,0));
+
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int buy = 0 ; buy<=1 ; buy++){
+                long profit = 0;
+                 if(buy){
+                    profit = max(-prices[i] + dp[i+1][0] , 0 + dp[i+1][1]);
+                }
+                else {
+                    profit = max(prices[i] - fee + dp[i+1][1],          // here is the only change
+                                  0 + dp[i+1][0]);
+                }
+
+                 dp[i][buy] = profit;
+            }
+        }
+        
+        return dp[0][1]; 
+    }
+};
