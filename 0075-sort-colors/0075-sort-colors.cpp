c1628fc634b73c72b58b4a67c94912a36cf2a57e
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-07-18 10:15:04 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (38.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 = 0 ;
-        int count1 = 0;
         int n = nums.size();
+        int t = 0 ;
+        int sum = 0;
         
-        
-        for(int  i = 0 ;i<n ; i++){
-            if(nums[i] == 0){
-                count0++;
-            }
+        for(int i = 0 ;i< n ; i++){
+            sum = sum+nums[i];
+            if(nums[i] == 2) t++;
         }
         
-        cout<<"0=>"<<count0<<endl;
+        int o = sum-t*2;
+        int z = n-o-t;
         
-        for(int  i = 0 ;i<n ; i++){
-            if(nums[i] == 1){
-                count1++;
-            }
+        for(int i = 0 ;i<n ;i++){
+            if(i<z){
+                nums[i] = 0 ;
+            }else if(i<z+o){
+                nums[i] = 1;
+            }else if(i<z+o+t) nums[i] = 2 ;
         }
-        
-         cout<<"1=>"<<count1<<endl;
-        
-        for(int i = 0 ;i<count0 ; i++){
-            nums.at(i) = 0;
-        }
-        
-            
-        for(int i = count0 ;i<count1+count0 ; i++){
-            nums.at(i) = 1;
-        }
-        
-            
-        for(int i = count0+count1 ;i<n ; i++){
-            nums.at(i) = 2;
-        }
-        
-        
-        
     }
 };
