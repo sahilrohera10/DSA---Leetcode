f149c54f55a3a96c3b023563e2ede13e97f5c25c
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-08-12 17:13:18 +0530

    Time: 60 ms (79.35%), Space: 41.2 MB (34.62%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,33 +3,72 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         
         int n = nums.size();
-       sort(nums.begin() , nums.end());
-
-       int i = 1 ;
-       int j = 0 ;
-
-       while(j<n){
-           if(j>0 && nums[j] == nums[j-1]){
-               j++;
-               continue;
-           }
-           if(nums[j] <= 0)
-             j++;  
-
-           else{
-               if(nums[j] == i){
-
-                   i++;
-                   j++;
-
-               }else {
-
-                    return i;
-               }
-
-           }
-       }
-
-       return i;
+      int i = 0 ;
+        while(i<n){
+            if(nums[i] > 0 && nums[i] != i+1 && nums[i] < n && nums[nums[i]-1] != nums[i]){
+                swap(nums[i] , nums[nums[i]-1]);
+            }else i++;
+        }
+        
+        for(int i = 1 ; i<=n ;i++){
+            if(nums[i-1] != i) return i;
+        }
+        
+        return n+1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////// Brute Force /////////////
+
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+        
+//         int n = nums.size();(
+     
+
+//        int i = 1 ;
+//        int j = 0 ;
+
+//        while(j<n){
+//            if(j>0 && nums[j] == nums[j-1]){
+//                j++;
+//                continue;
+//            }
+//            if(nums[j] <= 0)
+//              j++;  
+
+//            else{
+//                if(nums[j] == i){
+
+//                    i++;
+//                    j++;
+
+//                }else {
+
+//                     return i;
+//                }
+
+//            }
+//        }
+
+//        return i;
+//     }
+// };
