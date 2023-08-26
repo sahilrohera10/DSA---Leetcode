34904fad7370e3f30664794de8bb708ce355b04f
Sahil Rohera <90958499+sahilrohera10@users.noreply.github.com>
2023-08-27 00:00:37 +0530

    Added solution - LeetHub

---
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
+                     pair<int, int> destination) {
+                         
+                         
+        int n = grid.size();
+        int m = grid[0].size();
+                         
+                         
+        vector<vector<int>> dis(n , vector<int> (m , 1e8));
+        
+        grid[source.first][source.second] = 0;
+        
+        queue<pair<int , pair<int , int>>> q;
+        
+        q.push({0 , {source.first , source.second}});
+        
+        int delrow[] = {-1 , 0 , 1 , 0};
+        int delcol[] = {0 , 1 , 0 , -1};
+        
+        while(!q.empty())
+        {
+            int dist = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
+            q.pop();
+            if(row == destination.first && col == destination.second) return dist;
+            
+            
+            for(int i = 0 ;i<4 ; i++){
+                int nrow = row+delrow[i];
+                int ncol = col+delcol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1){
+                    if(dist + 1 < dis[nrow][ncol]) {
+                      dis[nrow][ncol] = dist+1;
+                      q.push({dist+1 , {nrow , ncol}});
+                        
+                    }
+                }
+            }
+            
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+
+        pair<int, int> source, destination;
+        cin >> source.first >> source.second;
+        cin >> destination.first >> destination.second;
+        Solution obj;
+        cout << obj.shortestPath(grid, source, destination) << endl;
+    }
+}
+
+// } Driver Code Ends
